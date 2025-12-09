a447d786c718d97a470e554414f48aea1be4c75a
root <root@delta.>
2025-12-09 09:06:09 +0000

    a

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -9,28 +9,8 @@ import Testimonials from "./Testimonials";
 import Careers from "./Careers";
 import Offices from "./Offices";
 import Footer from "./Footer";
-import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
 
 export default function HomeScreen() {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothWheel: true,
-    });
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
   return (
     <div className="bg-white relative w-full overflow-x-hidden">
       <main className="w-full">
